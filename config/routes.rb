99409dfe36f8990b763f42d62ebcdccd2144ee63
AzusaMorishita <184392623+azusaM0717@users.noreply.github.com>
2025-01-07 13:02:47 +0000

    nest_comments_to_reviews

--- config/routes.rb
+++ config/routes.rb
@@ -12,23 +12,24 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: "homes#top"
     resources :huts, only: [:new, :index, :show, :edit, :update]
-    resources :comments, only: [:index, :destroy]
-    resources :reviews, only: [:index, :destroy]
     resources :users, only: [:index, :show, :edit, :update]
+    resources :reviews, only: [:index, :destroy] do
+      resources :comments, only: [:index, :destroy]
+    end
   end
 
   scope module: :public do
     root to: "homes#top"
     get "/about" => "homes#about"
     resources :huts, only: [:index, :show]
+    resources :users, only: [:show, :edit, :update]
     resources :reviews do
       resources :comments, only: [:create, :destroy]
       resources :favorites, only: [:create, :destroy] 
     end
-    resources :users, only: [:show, :edit, :update]
     get "/mypage" => "users#show"
     get "/information/edit" => "users#edit"
-    patch "/information/edit" => "users#update"
+    patch "/users/edit" => "users#update"
     get "users/unsubscribe" => "users#unsubscribe"
     patch "users/withdraw" => "users#withdraw"
     get "users/favorites" => "users#favorites"
