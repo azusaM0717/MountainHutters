7312c1513f775c4102a87c517af306b083f1e233
AzusaMorishita <184392623+azusaM0717@users.noreply.github.com>
2025-01-07 05:23:03 +0000

    add_public_controller

---
+++ app/controllers/public/users_controller.rb
@@ -0,0 +1,16 @@
+class Public::UsersController < ApplicationController
+  def show
+  end
+
+  def eedit
+  end
+
+  def update
+  end
+
+  def unsubscribe
+  end
+
+  def withdraw
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,39 @@
 Rails.application.routes.draw do
   
 
+  namespace :public do
+    get 'favorites/create'
+    get 'favorites/destroy'
+    get 'favorites/index'
+  end
+  namespace :public do
+    get 'comments/create'
+    get 'comments/destroy'
+  end
+  namespace :public do
+    get 'huts/index'
+    get 'huts/show'
+  end
+  namespace :public do
+    get 'reviews/new'
+    get 'reviews/index'
+    get 'reviews/show'
+    get 'reviews/create'
+    get 'reviews/edit'
+    get 'reviews/update'
+    get 'reviews/destroy'
+  end
+  namespace :public do
+    get 'users/show'
+    get 'users/eedit'
+    get 'users/update'
+    get 'users/unsubscribe'
+    get 'users/withdraw'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   devise_for :users
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
