bca1d750c16ee7c9681c2b0f962cbc19173670df
AzusaMorishita <184392623+azusaM0717@users.noreply.github.com>
2025-01-07 05:58:31 +0000

    modify_routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,67 +1,34 @@
 Rails.application.routes.draw do
   
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
 
-  namespace :admin do
-    get 'huts/index'
-    get 'huts/new'
-    get 'huts/create'
-    get 'huts/show'
-    get 'huts/edit'
-    get 'huts/update'
-  end
-  namespace :admin do
-    get 'comments/index'
-    get 'comments/destroy'
-  end
-  namespace :admin do
-    get 'reviews/index'
-    get 'reviews/destroy'
-  end
-  namespace :admin do
-    get 'users/index'
-    get 'users/show'
-    get 'users/edit'
-    get 'users/update'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'favorites/create'
-    get 'favorites/destroy'
-    get 'favorites/index'
-  end
-  namespace :public do
-    get 'comments/create'
-    get 'comments/destroy'
-  end
-  namespace :public do
-    get 'huts/index'
-    get 'huts/show'
-  end
-  namespace :public do
-    get 'reviews/new'
-    get 'reviews/index'
-    get 'reviews/show'
-    get 'reviews/create'
-    get 'reviews/edit'
-    get 'reviews/update'
-    get 'reviews/destroy'
-  end
-  namespace :public do
-    get 'users/show'
-    get 'users/eedit'
-    get 'users/update'
-    get 'users/unsubscribe'
-    get 'users/withdraw'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  devise_for :users
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
-}
+  }
 
+  namespace :admin do
+    root to: "homes#top"
+    resources :huts, only: [:new, :index, :show, :edit, :update]
+    resources :comments, only: [:index, :destroy]
+    resources :reviews, only: [:index, :destroy]
+    resources :users, only: [:index, :show, :edit, :update]
+  end
+
+  scope module: :public do
+    root to: "homes#top"
+    get "/about"=>"homes#about"
+    resources :favorites, only: [:create, :destroy, :index]
+    resources :comments, only: [:create, :destroy]
+    resources :huts, only: [:index, :show]
+    resources :reviews
+    resources :users, only: [:show, :edit, :update]
+    get "/mypage"=>"users#show"
+    get "/information/edit"=>"users#edit"
+    patch "/information/edit"=>"users#update"
+    get "users/unsubscribe"=>"users#unsubscribe"
+    patch "users/withdraw" => "users#withdraw"
+  end
 end
